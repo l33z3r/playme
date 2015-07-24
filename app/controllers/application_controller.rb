5165152f749ff0a59c0e1c642f4fbca133000b84
Lee Farrell <lee.farrell86@gmail.com>
2015-07-24 10:10:14 +0100

    bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, :except => [:device_verify]
 
   before_action :authenticate_user!, except: [:login_attempt, :login, :device_verify]
 end
