a22da0bf586704fc33131794e01a4dcc838c2dc8
Lee Farrell <lfarrell@fusioneer.com>
2015-10-01 11:57:33 +0100

    added pry and more spotify permissions

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,5 +1,6 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, '8f6facc3c8724b42b361675473c555b7', 'f127b9fda8fc4927bcf14272e2746677', scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, '8f6facc3c8724b42b361675473c555b7', 'f127b9fda8fc4927bcf14272e2746677',
+           scope: 'playlist-read-private user-read-birthdate playlist-read-collaborative user-library-read user-library-modify user-read-private user-read-email playlist-modify-public user-library-read user-library-modify'
 end
